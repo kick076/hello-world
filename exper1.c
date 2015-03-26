3c3b305c8b46aee69113ca20b858c64fd79afbd8
kick076 <bollavularajasekhar@gmail.com>
2015-03-27 00:59:39 +0530

    added expr1.c

---
+++ exper1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<signal.h>
+void signalhandler(int signum)
+{
+printf("received signal number is %d\n",signum);
+exit(signum);
+}
+int main()
+{
+signal(SIGINT,signalhandler);
+while(1)
+{
+printf("welcome to ui lab \n");
+sleep(1);
+}
+return EXIT_SUCCESS;
+}
